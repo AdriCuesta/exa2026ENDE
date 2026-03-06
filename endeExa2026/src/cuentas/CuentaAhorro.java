42c6e64f319c4dc367eb80f396055580e24d64e9
Adrian Cuesta <AdrianCuesta@ELP69>
2026-03-06 13:19:32 +0100

    feat(CuentaAhorro): calcularInteres

---
+++ endeExa2026/src/cuentas/CuentaAhorro.java
@@ -0,0 +1,16 @@
+package cuentas;
+
+public class CuentaAhorro extends CuentaBancaria {
+
+	private double interes = 0.23;
+
+	public CuentaAhorro(String titular, double saldoInicial, double interes) {
+		super(titular, saldoInicial);
+		this.interes = interes;
+	}
+
+	@Override
+	public double calcularInteres() {
+		return (saldo * interes);
+	}
+}
